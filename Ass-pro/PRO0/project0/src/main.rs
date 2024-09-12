6b217ac3bdbc5f7a166510d5d19f40505c8dc092
Wasted_Marl <ilu.raghus@icloud.com>
2024-09-12 12:50:26 -0600

    pro1

--- Ass-pro/PRO0/project0/src/main.rs
+++ Ass-pro/PRO0/project0/src/main.rs
@@ -1,8 +1,42 @@
-use plotly::{Layout, Scatter, ImageFormat, Plot};
+use plotly::common::{Mode,Title};
+use plotly::layout::{Layout};
+use plotly::{Plot, ImageFormat};
+use plotly::Scatter;
+use std::ops::RangeInclusive;
+
+// defining the two functions
+fn f(x:i32) -> i32{
+    (0.5 * x as f64 * (x as f64 - 1.0) + 10.0) as i32
+}
+
+//it panick, I believe due to this, can i32 store a value or a square of 10000000 ??? 
+//Need more info on this. 
+
+fn g(x:i32) -> i32{
+    x.pow(2)
+}
+
+fn plot(range: RangeInclusive<i32>,filename: &str, title: &str, step: usize) {
+    let x_vals: Vec<i32> = range.step_by(step).collect();
+    let f_vals: Vec<i32> = x_vals.iter().map(|&n| f(n)).collect(); //output of the functionn f(x)
+    let g_vals: Vec<i32> = x_vals.iter().map(|&n| g(n)).collect(); //output of the fucntion g(x)
+
+    //trace of f valus
+    let trace1=Scatter::new(x_vals.clone(), f_vals).mode(Mode::Lines).name("f(n)");
+    let trace2=Scatter::new(x_vals.clone(),g_vals).mode(Mode::Lines).name("g(n)");
+
+    let mut plot= Plot::new();
+    plot.add_trace(trace1);
+    plot.add_trace(trace2);
+
+    let layout = Layout::new().title(Title::with_text(title)).x_axis(plotly::layout::Axis::new().title("n")).y_axis(plotly::layout::Axis::new().title("Function values"));
+    plot.set_layout(layout);
+    plot.write_html(filename);
+
+}
 
 fn main(){
-    let mut plot = Plot::new();
-    let trace = Scatter::new(vec![0, 1, 2], vec![2, 1, 0]);
-    plot.add_trace(trace);
-    plot.write_image("out.png", ImageFormat::PNG, 800, 600, 1.0);
+    plot(1..=10, "plot_1_to_10.html", "Functions f(n) and g(n) for n ranging from 1 to 10",1);
+    // Plot for n ranging from 1 to 10^6
+    plot(1..=100000, "plot_1_to_1000000.html", "Functions f(n) and g(n) for n ranging from 1 to 10^6",1000);
 }
