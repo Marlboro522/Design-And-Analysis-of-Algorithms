b9b62772252264fea85e4b452f00481d1cfdab90
Wasted_Marl <ilu.raghus@icloud.com>
2024-09-12 13:33:51 -0600

    pro1-Working and needs optimization

--- Ass-pro/PRO0/project0/src/main.rs
+++ Ass-pro/PRO0/project0/src/main.rs
@@ -5,21 +5,21 @@ use plotly::Scatter;
 use std::ops::RangeInclusive;
 
 // defining the two functions
-fn f(x:i32) -> i32{
-    (0.5 * x as f64 * (x as f64 - 1.0) + 10.0) as i32
+fn f(x:i128) -> i128{
+    (0.5 * x as f32 * (x as f32 - 1.0) + 10.0) as i128
 }
 
 //it panick, I believe due to this, can i32 store a value or a square of 10000000 ??? 
 //Need more info on this. 
 
-fn g(x:i32) -> i32{
+fn g(x:i128) -> i128{
     x.pow(2)
 }
 
-fn plot(range: RangeInclusive<i32>,filename: &str, title: &str, step: usize) {
-    let x_vals: Vec<i32> = range.step_by(step).collect();
-    let f_vals: Vec<i32> = x_vals.iter().map(|&n| f(n)).collect(); //output of the functionn f(x)
-    let g_vals: Vec<i32> = x_vals.iter().map(|&n| g(n)).collect(); //output of the fucntion g(x)
+fn plot(range: RangeInclusive<i128>,filename: &str, title: &str, step: usize) {
+    let x_vals: Vec<i128> = range.step_by(step).collect();
+    let f_vals: Vec<i128> = x_vals.iter().map(|&n| f(n)).collect(); //output of the functionn f(x)
+    let g_vals: Vec<i128> = x_vals.iter().map(|&n| g(n)).collect(); //output of the fucntion g(x)
 
     //trace of f valus
     let trace1=Scatter::new(x_vals.clone(), f_vals).mode(Mode::Lines).name("f(n)");
@@ -38,5 +38,5 @@ fn plot(range: RangeInclusive<i32>,filename: &str, title: &str, step: usize) {
 fn main(){
     plot(1..=10, "plot_1_to_10.html", "Functions f(n) and g(n) for n ranging from 1 to 10",1);
     // Plot for n ranging from 1 to 10^6
-    plot(1..=100000, "plot_1_to_1000000.html", "Functions f(n) and g(n) for n ranging from 1 to 10^6",1000);
+    plot(1..=1000000, "plot_1_to_1000000.html", "Functions f(n) and g(n) for n ranging from 1 to 10^6",1000);
 }
